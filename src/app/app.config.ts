83f4bcf1a3d254e130ce3f100725416c4e1bf603
Luis Fernandez <luisfernandezvega@hotmail.com>
2026-06-17 15:16:29 -0400

    login and fixes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,9 +8,19 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), 
-    provideFirebaseApp(() => 
-      initializeApp( environment.firebase)), 
-        provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp( environment.firebase)),
+        provideAuth(() => getAuth()),
+        provideFirestore(() => getFirestore()),
+        provideFirebaseApp(() => initializeApp(
+          {
+            projectId: "stock-app-b9475",
+            appId: "1:602421702706:web:97855ec83612f939198c2b",
+            storageBucket: "stock-app-b9475.firebasestorage.app",
+            apiKey: "AIzaSyCIspo0QkSjuu5nspIeDuG512pl3s9uRzE",
+            authDomain: "stock-app-b9475.firebaseapp.com",
+            messagingSenderId: "602421702706",
+          })), provideFirestore(() => getFirestore())]
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,12 @@
 export const environment = {
 
    firebase: {
-      projectId: "stock-app-b9475", 
-      appId: "1:602421702706:web:97855ec83612f939198c2b", 
-      storageBucket: "stock-app-b9475.firebasestorage.app", 
-      apiKey: "AIzaSyCIspo0QkSjuu5nspIeDuG512pl3s9uRzE", 
-      authDomain: "stock-app-b9475.firebaseapp.com", 
-      messagingSenderId: "602421702706", 
-      projectNumber: "602421702706", 
-      version: "2" 
+      apiKey: "AIzaSyCIspo0QkSjuu5nspIeDuG512pl3s9uRzE",
+      authDomain: "stock-app-b9475.firebaseapp.com",
+      projectId: "stock-app-b9475",
+      storageBucket: "stock-app-b9475.firebasestorage.app",
+      messagingSenderId: "602421702706",
+      appId: "1:602421702706:web:97855ec83612f939198c2b"
    }
 
 
