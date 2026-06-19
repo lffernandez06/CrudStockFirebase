64b718916caf851c3cc5771a3a220a8dee6cab0a
Luis Fernandez <luisfernandezvega@hotmail.com>
2026-06-19 19:27:34 -0400

    Auto-generated commit

---
+++ chunk-OASMGU5H.js
@@ -0,0 +1,2 @@
+import{Na as o,db as a,eb as i,nb as e}from"./chunk-J3XGZFUR.js";var s=class n{static \u0275fac=function(t){return new(t||n)};static \u0275cmp=o({type:n,selectors:[["app-not-found"]],decls:3,vars:0,template:function(t,p){t&1&&(a(0,"p"),e(1,"not-found works!"),i(),e(2,`,
+`))},encapsulation:2,changeDetection:0})};export{s as NotFound};
